b001dd07be7da2931a9c473a5466fb2c64a3d51f
Peter Gerrard <32jezza@gmail.com>
2015-04-13 16:42:58 +0100

    Created a Play Area.

---
+++ DominionTDD.Tests/PlayAreaTests.cs
@@ -0,0 +1,95 @@
+﻿using System.Linq;
+using DominionTDD.Cards;
+using NUnit.Framework;
+
+namespace DominionTDD.Tests
+{
+    [TestFixture]
+    public class PlayAreaTests
+    {
+        private PlayArea _playArea;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _playArea = new PlayArea();
+        }
+
+        [Test]
+        public void PlayAreaStartsEmpty()
+        {
+            // ASSERT
+            Assert.That(_playArea.Cards(), Is.Empty);
+        }
+
+        [Test]
+        public void CanSeeASingleCardAddedIntoThePlayArea()
+        {
+            // ARRANGE
+            var copper = new Copper();
+
+            // ACT
+            _playArea.AddCard(copper);
+
+            // ASSERT
+            Assert.That(_playArea.Cards(), Is.EquivalentTo(new []{copper}));
+        }
+
+        [Test]
+        public void CanSeeAllCardsWhenMultipleAdded()
+        {
+            // ARRANGE
+            var copper = new Copper();
+            var silver = new Silver();
+
+            // ACT
+            _playArea.AddCard(copper);
+            _playArea.AddCard(silver);
+
+            // ASSERT
+            Assert.That(_playArea.Cards(), Is.EquivalentTo(new ICard[] { copper, silver }));
+        }
+
+        [Test]
+        public void RemovingAllCardsWhenEmptyReturnsEmptySet()
+        {  
+            // ACT
+            var removed = _playArea.TakeAll();
+
+            // ASSERT
+            Assert.That(removed, Is.Empty);
+        }
+
+        [Test]
+        public void RemovingMultipleCardsGetsAllTheCards()
+        {
+            // ARRANGE
+            var copper = new Copper();
+            var silver = new Silver();
+            _playArea.AddCard(copper);
+            _playArea.AddCard(silver);
+
+            // ACT
+            var removed = _playArea.TakeAll();
+
+            // ASSERT
+            Assert.That(removed, Is.EquivalentTo(new ICard[] { copper, silver }));
+        }
+
+        [Test]
+        public void TakingAllCardsLeavesItEmpty()
+        {
+            // ARRANGE
+            var copper = new Copper();
+            var silver = new Silver();
+            _playArea.AddCard(copper);
+            _playArea.AddCard(silver);
+
+            // ACT
+            _playArea.TakeAll();
+
+            // ASSERT
+            Assert.That(_playArea.Cards(), Is.Empty);
+        }
+    }
+}
---
+++ DominionTDD/PlayArea.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using DominionTDD.Cards;
+
+namespace DominionTDD
+{
+    public class PlayArea
+    {
+        private readonly IList<ICard> _cards = new List<ICard>();
+
+        public IEnumerable<ICard> Cards()
+        {
+            return _cards;
+        }
+
+        public void AddCard(ICard card)
+        {
+            _cards.Add(card);
+        }
+
+        public IEnumerable<ICard> TakeAll()
+        {
+            var taken = new List<ICard>(_cards);
+            _cards.Clear();
+            return taken;
+        }
+    }
+}
